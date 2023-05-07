edde9570bb0ae2f6313a71f6962a705d5bdba3e7
meganbougle <mmbougle@uamv.edu.ni>
2023-05-06 21:41:30 -0600

    Fonts,Imagenes y Strings agregados

--- OMWAYAppViews/OMWayApp/app/src/main/java/com/main/omwayapp/ui/theme/Type.kt
+++ OMWAYAppViews/OMWayApp/app/src/main/java/com/main/omwayapp/ui/theme/Type.kt
@@ -2,16 +2,33 @@ package com.main.omwayapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.main.omwayapp.R
 
+val IBMplexSans = FontFamily(
+    Font(R.font.ibmplexsans_bold, FontWeight.Bold),
+    Font(R.font.ibmplexsans_light, FontWeight.Light),
+    Font(R.font.ibmplexsans_regular, FontWeight.Normal),
+    Font(R.font.ibmplexsans_semibold, FontWeight.SemiBold),
+    Font(R.font.imbplexsans_medium, FontWeight.Medium)
+)
+
+val Inter = FontFamily(
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_regular, FontWeight.Normal),
+)
 // Set of Material typography styles to start with
-val Typography = Typography(
+val AppTypography1 = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = IBMplexSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontSize = 50.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
