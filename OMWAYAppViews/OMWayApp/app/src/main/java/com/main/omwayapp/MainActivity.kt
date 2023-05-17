5da87d947d6d5065da85e2f05a2b1371407a9241
CarlosFCerdaE <111518251+CarlosFCerdaE@users.noreply.github.com>
2023-05-17 14:38:04 -0600

    Update MainActivity.kt

--- OMWAYAppViews/OMWayApp/app/src/main/java/com/main/omwayapp/MainActivity.kt
+++ OMWAYAppViews/OMWayApp/app/src/main/java/com/main/omwayapp/MainActivity.kt
@@ -80,7 +80,7 @@ class MainActivity : ComponentActivity() {
                     //DriverView()
                     //RiderView()
                     //BttmSheet()
-                    MapView()
+                    //MapView()
 
                 }
 
