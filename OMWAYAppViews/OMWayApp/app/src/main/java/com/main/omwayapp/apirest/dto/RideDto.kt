3543205bfd8917ff39e062e18d1239b90ba032b3
Carlos Cerda <cfcerda@uamv.edu.ni>
2023-06-01 00:13:20 -0600

    Creacion Dto para servicio

--- OMWAYAppViews/OMWayApp/app/src/main/java/com/main/omwayapp/apirest/dto/RideDto.kt
+++ OMWAYAppViews/OMWayApp/app/src/main/java/com/main/omwayapp/apirest/dto/RideDto.kt
@@ -1,30 +1,33 @@
-package com.main.omwayapp.apirest.model
+package com.main.omwayapp.apirest.dto
 
-import java.sql.Time
-import java.time.LocalDate
 import android.os.Parcelable
+import com.main.omwayapp.apirest.model.DriverItem
+import com.main.omwayapp.apirest.model.PaymentCashItem
+import com.main.omwayapp.apirest.model.RiderItem
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.sql.Time
+import java.time.LocalDate
 
 @Parcelize
 @Serializable
-data class RideItem(
+data class RideDto(
     val id:Int,
     @Contextual
     val pickUpTime: Time,
     @Contextual
-    val dropOffTime:Time,
+    val dropOffTime: Time,
     val pickUpLocation:String,
     val dropOffLocation:String,
     val distance:Double,
     @Contextual
-    val date:LocalDate,
+    val date: LocalDate,
     val notes:String,
     val state: String,
     val fare: Double,
     val rating:Int,
     val comment:String,
-    val rider:RiderItem,
-    val driver:DriverItem
+    val riderId: String,
+    val driverId:String
 ) : Parcelable
